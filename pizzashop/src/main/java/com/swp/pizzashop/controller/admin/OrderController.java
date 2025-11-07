7e8a29d9bb4bc3d58560b8945f576d02d3e067c5
VuBichNgoc <vubichngoc30082002@gmail.com>
2025-11-07 11:14:08 +0700

    Update OrderController logic

--- pizzashop/src/main/java/com/swp/pizzashop/controller/admin/OrderController.java
+++ pizzashop/src/main/java/com/swp/pizzashop/controller/admin/OrderController.java
@@ -1,4 +1,17 @@
 package com.swp.pizzashop.controller.admin;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/orders")
+@RequiredArgsConstructor
+@Slf4j
 public class OrderController {
+    @RequestMapping
+    public String orders() {
+        return "admin/orders";
+    }
 }
