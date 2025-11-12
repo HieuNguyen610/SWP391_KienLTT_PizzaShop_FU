72d81db43f4b0aae5172294af56dea9c05a54d87
hieunm2 <hieunm2@vnext.vn>
2025-11-12 14:20:30 +0700

    using EntityGraph to avoid lazy fetch category

--- pizzashop/src/main/java/com/swp/pizzashop/repository/FoodRepository.java
+++ pizzashop/src/main/java/com/swp/pizzashop/repository/FoodRepository.java
@@ -4,6 +4,7 @@ import com.swp.pizzashop.model.Food;
 import com.swp.pizzashop.model.FoodCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,18 +13,23 @@ import java.util.List;
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
+    @EntityGraph(attributePaths = "category")
     List<Food> findByIsDeletedFalseOrderByIdDesc();
 
     long countByCategoryAndIsDeletedFalse(FoodCategory category);
 
     long countByCategoryAndIsActiveTrueAndIsDeletedFalse(FoodCategory category);
 
+    @EntityGraph(attributePaths = "category")
     Page<Food> findByIsDeletedFalse(Pageable pageable);
 
+    @EntityGraph(attributePaths = "category")
     Page<Food> findByIsDeletedFalseAndNameContainingIgnoreCase(String name, Pageable pageable);
 
+    @EntityGraph(attributePaths = "category")
     Page<Food> findByIsDeletedFalseAndCategoryId(Long categoryId, Pageable pageable);
 
+    @EntityGraph(attributePaths = "category")
     Page<Food> findByIsDeletedFalseAndCategoryIdAndNameContainingIgnoreCase(Long categoryId, String name, Pageable pageable);
 
     List<Food> findByCategoryAndIsDeletedFalse(FoodCategory category);
